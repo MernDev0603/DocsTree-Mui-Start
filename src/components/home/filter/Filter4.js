73c26df2c0d54521a7bf5411dd3325ddcd7c4e24
K&K <95503551+MernDev0603@users.noreply.github.com>
2022-10-15 23:59:12 -0400

    filter4

--- src/components/home/filter/Filter4.js
+++ src/components/home/filter/Filter4.js
@@ -1,45 +1,13 @@
-import { useState } from 'react'
-import { styled, useTheme } from '@mui/material/styles';
-import { Box, InputLabel, Stack, Select, MenuItem, Divider, FormControlLabel, FormGroup, FormControl, TextField, Checkbox } from '@mui/material';
+import { Icon } from '@iconify/react';
+import { styled } from '@mui/material/styles';
+import { Radio, RadioGroup, FormControlLabel, FormControl, Stack } from '@mui/material';
 // -------------------------------------------------------------
-const RootStyle = styled('div')(({ theme }) => ({
-    width: '100%'
-}));
-
-const ContainerStyle = styled('div')(({ theme }) => ({
-    border: '1px solid #E2E4E5',
-    borderRadius: '7px',
-    padding: '10px'
-}));
-
-const InnerContainerStyle = styled('div')(({ theme }) => ({
-    width: '65%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-}));
-
-const LineStyle = styled('div')(({ theme }) => ({
-    width: '100%',
-    height: '1px',
-    backgroundColor: '#C6C6C6'
-}));
-
-const HeaderBTNStyle = styled('button')({
-    color: '#17686E',
-    border: '1px solid #17686E',
-    borderRadius: '5px',
-    backgroundColor: '#fff',
-    padding: '10px'
-});
-
 const FilterTitleStyle = styled('p')({
     color: '#313131',
     fontFamily: 'Poppins',
     fontWeight: 500,
     fontSize: '14px',
 });
-
 // -------------------------------------------------------------
 
 export default function Filter4() {
@@ -47,6 +15,119 @@ export default function Filter4() {
     return (
         <>
             <FilterTitleStyle>Avaliação do Cliente</FilterTitleStyle>
+            <FormControl>
+                <RadioGroup
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    defaultValue="5"
+                    name="radio-buttons-group"
+                >
+                    <FormControlLabel value="5"
+                        control={
+                            <Radio
+                                sx={{
+                                    '& .MuiSvgIcon-root': { fontSize: 16 },
+                                    color: '#17686E',
+                                    '&.Mui-checked': {
+                                        color: '#17686E',
+                                    },
+                                    '& .css-1l5uw3e-MuiButtonBase-root-MuiRadio-root': {
+                                        padding: '5px'
+                                    }
+                                }}
+                            />}
+                        label={
+                            <Stack direction="row">
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                            </Stack>
+                        }
+                    />
+                    <FormControlLabel value="4"
+                        control={
+                            <Radio
+                                sx={{
+                                    '& .MuiSvgIcon-root': { fontSize: 16 },
+                                    color: '#17686E',
+                                    '&.Mui-checked': {
+                                        color: '#17686E',
+                                    }
+                                }}
+                            />
+                        }
+                        label={
+                            <Stack direction="3">
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                            </Stack>
+                        }
+                    />
+                    <FormControlLabel value="2"
+                        control={<Radio
+                            sx={{
+                                '& .MuiSvgIcon-root': { fontSize: 16 },
+                                color: '#17686E',
+                                '&.Mui-checked': {
+                                    color: '#17686E',
+                                }
+                            }}
+                        />} label={
+                            <Stack direction="row">
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                            </Stack>
+                        }
+                    />
+                    <FormControlLabel value="1"
+                        control={<Radio
+                            sx={{
+                                '& .MuiSvgIcon-root': { fontSize: 16 },
+                                color: '#17686E',
+                                '&.Mui-checked': {
+                                    color: '#17686E',
+                                }
+                            }}
+                        />}
+                        label={
+                            <Stack direction="row">
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                            </Stack>
+                        }
+                    />
+                    <FormControlLabel value="other"
+                        control={<Radio
+                            sx={{
+                                '& .MuiSvgIcon-root': { fontSize: 16 },
+                                color: '#17686E',
+                                '&.Mui-checked': {
+                                    color: '#17686E',
+                                }
+                            }}
+                        />}
+                        label={
+                            <Stack direction="row">
+                                <Icon icon="el:star" color="#17686e" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                                <Icon icon="bytesize:star" color="#c6c6c6" width="22" />
+                            </Stack>
+                        }
+                    />
+                </RadioGroup>
+            </FormControl>
         </>
     )
 }
